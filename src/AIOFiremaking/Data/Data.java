afcef27bd76cf9bf81b3f40c0be4a79f0aae49a3
IR0N <kuroxasch@gmail.com>
2019-01-17 03:59:39 -0500

    Initial commit

---
+++ src/AIOFiremaking/Data/Data.java
@@ -0,0 +1,18 @@
+package AIOFiremaking.Data;
+
+import xobot.script.wrappers.Tile;
+
+public class Data {
+    public static int logs_ID = 0;
+    public static String selectedLog = "";
+    public static String selectedArea = "";
+    public static final int[] booth_ID = {2213, 26972, 11758};
+
+    public static final Tile fireTileDZ1 = new Tile(2732, 3486);
+    public static final Tile fireTileDZ2 = new Tile(2732, 3485);
+    public static final Tile fireTileEdgeville1 = new Tile(3104, 3486);
+    public static final Tile fireTileEdgeville2 = new Tile(3104, 3485);
+    public static final Tile fireTileEdgeville3 = new Tile(3104, 3484);
+    public static final Tile bankTileDZ = new Tile(2540, 3889);
+    public static final Tile bankTileEdgeville = new Tile(3094, 3491);
+}
---
+++ src/AIOFiremaking/Utils/gui.java
@@ -0,0 +1,131 @@
+package AIOFiremaking.Utils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.Objects;
+import AIOFiremaking.Data.Data;
+
+public class gui extends JFrame {
+
+    public gui() {
+        initComponents();
+    }
+
+    private void startButtonActionPerformed(ActionEvent e) {
+        switch ((String)(Objects.requireNonNull(locationSelector.getSelectedItem()))){
+            case "Donator Zone":
+                Data.selectedArea = "Donator Zone";
+                break;
+            case "Edgeville":
+                Data.selectedArea = "Edgeville";
+                break;
+        }
+        switch ((String) Objects.requireNonNull(logSelector.getSelectedItem())) {
+            case "Logs":
+                Data.logs_ID = 1511;
+                break;
+            case "Oak logs":
+                Data.logs_ID = 1521;
+                break;
+            case "Willow logs":
+                Data.logs_ID = 1519;
+                break;
+            case "Maple logs":
+                Data.logs_ID = 1517;
+                break;
+            case "Yew logs":
+                Data.logs_ID = 1515;
+                break;
+            case "Magic logs":
+                Data.logs_ID = 1513;
+                break;
+        }
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Christopher Edwards
+        label1 = new JLabel();
+        locationSelector = new JComboBox<>();
+        logSelector = new JComboBox<>();
+        startButton = new JButton();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+
+        //---- label1 ----
+        label1.setText("AIO Firemaking");
+        label1.setFont(new Font("Ink Free", Font.BOLD, 28));
+
+        //---- locationSelector ----
+        locationSelector.setModel(new DefaultComboBoxModel<>(new String[]{
+                "Select a Location..",
+                "Donator Zone",
+                "Edgeville"
+        }));
+        locationSelector.setFont(new Font("Ink Free", Font.BOLD, 16));
+
+        //---- logSelector ----
+        logSelector.setModel(new DefaultComboBoxModel<>(new String[]{
+                "Select a Log..",
+                "Logs",
+                "Oak logs",
+                "Willow logs",
+                "Maple logs",
+                "Yew logs",
+                "Magic logs"
+        }));
+        logSelector.setFont(new Font("Ink Free", Font.BOLD, 16));
+        logSelector.addActionListener(e -> Data.selectedLog = Objects.requireNonNull(logSelector.getSelectedItem()).toString());
+        locationSelector.addActionListener(e -> Data.selectedArea = Objects.requireNonNull(locationSelector.getSelectedItem()).toString());
+        //---- startButton ----
+        startButton.setText("Start");
+        startButton.setFont(new Font("Ink Free", Font.BOLD, 22));
+        startButton.addActionListener(this::startButtonActionPerformed);
+
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+                contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                .addGap(64, 64, 64)
+                                .addGroup(contentPaneLayout.createParallelGroup()
+                                        .addComponent(label1, GroupLayout.PREFERRED_SIZE, 229, GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
+                                                .addComponent(logSelector, GroupLayout.Alignment.LEADING)
+                                                .addComponent(locationSelector, GroupLayout.Alignment.LEADING)))
+                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(startButton)
+                                .addGap(49, 49, 49))
+        );
+        contentPaneLayout.setVerticalGroup(
+                contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                .addGap(31, 31, 31)
+                                .addComponent(label1, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+                                .addGap(42, 42, 42)
+                                .addComponent(locationSelector, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(logSelector, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                                .addComponent(startButton)
+                                .addGap(24, 24, 24))
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Christopher Edwards
+    private JLabel label1;
+    private JComboBox<String> locationSelector;
+    private JComboBox<String> logSelector;
+    private JButton startButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+
+}
